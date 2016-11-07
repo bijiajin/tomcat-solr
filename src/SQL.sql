0988aaf96f8728b5368a8b948d4fc60cdcf66c3d
hejiawang <952327407@qq.com>
2016-11-07 10:41:22 +0800

    solr 6.2.1 tomcat mysql

---
+++ src/SQL.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `solrTest` (
+  `solrID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `context` varchar(255) DEFAULT NULL COMMENT 'context',
+  `updateTime` datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'updateTime',
+  PRIMARY KEY (`ID`)
+) DEFAULT CHARSET=utf8;
