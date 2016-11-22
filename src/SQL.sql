8f26e5ac5e25e16c3f7b4f34a58fb04e2ea7bdf2
hejiawang <952327407@qq.com>
2016-11-22 17:00:47 +0800

    add filter

--- src/SQL.sql
+++ src/SQL.sql
@@ -2,5 +2,6 @@ CREATE TABLE `solrTest` (
   `solrID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `context` varchar(255) DEFAULT NULL COMMENT 'context',
   `updateTime` datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'updateTime',
+  `sort` int(11) DEFAULT '1' COMMENT '≈≈–Ú',
   PRIMARY KEY (`ID`)
 ) DEFAULT CHARSET=utf8;
